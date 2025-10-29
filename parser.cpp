0ce3ba9b1779515cac4a0072ef6ead7348a0fabd
Fatih Özdal <141767198+fatih-ozdal@users.noreply.github.com>
2025-10-29 10:02:03 +0300

    add nlohmann/json.hpp

--- parser.cpp
+++ parser.cpp
@@ -1,7 +1,15 @@
 #include "parser.h"
+#include <fstream>
+#include <nlohmann/json.hpp>
+
+using json = nlohmann::json;
 
 Scene parser::loadFromJson(const string &filepath)
 {
+    std::ifstream f(filepath);
+    json data = json::parse(f);
+
+
     Scene s;
     return s;
 }
--- raytracer.cpp
+++ raytracer.cpp
@@ -1,6 +1,14 @@
 #include "parser.h"
 
-int main()
+int main(int argc, char* argv[])
 {
+    if (argc < 2) {
+        printf("Usage: %s <scene.json>\n", argv[0]);
+        return -1;
+    }
+    
+    parser p;
+    string jsonFile = argv[1];
+    Scene scene = p.loadFromJson(jsonFile);
     return 0;
 }
